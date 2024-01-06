ace85e7b013a3ed513d7fe7ff55a803866110905
rizan1998 <rizanalfalah@gmail.com>
2024-01-06 22:13:54 +0700

    tipe data primitif typescript

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Data Type", function () {
+    it("should must declare", function () {
+        const name = "Rijan Alpalah";
+        const balance = 10000;
+        const isVip = true;
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,10 @@
+describe("Data Type", function () {
+  it("should must declare", function () {
+    const name: string = "Rijan Alpalah";
+    const balance: number = 10000;
+    const isVip: boolean = true;
+    console.log(name);
+    console.log(balance);
+    console.log(isVip);
+  });
+});
