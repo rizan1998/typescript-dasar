93633d660dbe13676dfce6d39052aa7ceadb2f33
rizan1998 <rizanalfalah@gmail.com>
2024-01-07 20:18:48 +0700

    union type data

---
+++ dist/tests/union.test.js
@@ -0,0 +1,27 @@
+"use strict";
+describe("Union Type", function () {
+    it("should support in typescript", function () {
+        let sample = "rijan";
+        console.info(sample);
+        sample = 100;
+        console.info(sample);
+        sample = true;
+        console.info(sample);
+    });
+    it("should support typeof operator", function () {
+        function process(value) {
+            if (typeof value === "string") {
+                return value.toLocaleUpperCase();
+            }
+            else if (typeof value === "number") {
+                return value + 2;
+            }
+            else {
+                return !value;
+            }
+        }
+        expect(process("Rijan")).toBe("Rijan");
+        expect(process(100)).toBe(102);
+        expect(process(true)).toBe(false);
+    });
+});
---
+++ tests/union.test.ts
@@ -0,0 +1,28 @@
+describe("Union Type", function () {
+  it("should support in typescript", function () {
+    let sample: number | string | boolean = "rijan";
+    console.info(sample);
+
+    sample = 100;
+    console.info(sample);
+
+    sample = true;
+    console.info(sample);
+  });
+
+  it("should support typeof operator", function () {
+    function process(value: number | string | boolean) {
+      if (typeof value === "string") {
+        return value.toLocaleUpperCase();
+      } else if (typeof value === "number") {
+        return value + 2;
+      } else {
+        return !value;
+      }
+    }
+
+    expect(process("Rijan")).toBe("Rijan");
+    expect(process(100)).toBe(102);
+    expect(process(true)).toBe(false);
+  });
+});
